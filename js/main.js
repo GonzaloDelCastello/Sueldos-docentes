adf4131a50b7d688cf4122d65f29d95ae189523d
Gonzalo Del Castello <delcastello.gonzalo@sanluis.edu.ar>
2025-04-18 21:27:54 -0300

    Primeras pruebas del JS

--- js/main.js
+++ js/main.js
@@ -5,48 +5,69 @@ function mostrarResultado() {
 
   var resultados = calcularSalario();
   document.getElementById("resultadoSueldo").textContent =
-    "Siendo tu sueldo imponible de $" + resultados.imponible;
+    "Sueldo básico por hs $" + resultados.imponible;
+  document.getElementById("resultadoDescuentos").textContent = 
+    "Pago por Zona: " + bonificacionZona;
   // document.getElementById("resultadoDescuentos").textContent = "Descuentos Legales: $" + resultados.descuentos
   // document.getElementById("resultadoLiquido").textContent = "Tu Sueldo líquido es de $" + resultados.liquido
 }
+
 function calcularSalario() {
   //var sueldoBruto = parseInt(document.getElementById("sueldoBase").value)
   let cantHs = parseInt(document.getElementById("cantHs").value);
   //var gratificacion = document.getElementById("gratificacion").value
 
-  let tipoCargo = (() => {
+  let calculoBasico = (() => {
     switch (document.getElementById("cargo").value) {
       case "1":
-        return { pagoXHs : 1000 };
+        return { pagoXHs: 12756.59 };
       case "2":
-        return { pagoXHs : 10 };
+        return { pagoXHs: 10 };
       default:
         return { pagoXHs: 0 };
     }
   })();
 
-//   var montoGratificacion = 0;
-//   if (gratificacion != "no") {
-//     montoGratificacion = calcularGratificacion(sueldoBruto);
-//   }
-//   var bonoMov = parseInt(document.getElementById("bonoMovilizacion").value);
-//   var bonoCol = parseInt(document.getElementById("bonoColacion").value);
-//   var afp = parseFloat(document.getElementById("afp").value).toFixed(2) / 100;
-//   afp = (afp + 10) / 100;
-//   var prevision = document.getElementById("prevision").value;
-//   var contrato = document.getElementById("tipocontrato").value;
+  function calculoZona() {
+    switch (document.getElementById("zona").value) {
+      case "1":
+        return { pagoXZona: 0 };
+      case "2":
+        return { pagoXZona: 20 };
+      case "3":
+        return { pagoXZona: 40 };
+      case "4":
+        return { pagoXZona: 60 };
+      case "5":
+        return { pagoXZona: 80 };
+      default:
+        return { pagoXHs: 0 };
+    }
+  }
 
-  var sueldoImponible = cantHs * tipoCargo.pagoXHs;
-//   var descuentosLegales = sueldoImponible * afp + sueldoImponible * 0.07;
-//   var sueldoLiquido = parseInt(sueldoImponible - descuentosLegales);
-//   if (sueldoLiquido < 100000) {
-//     alert(
-//       "Es posible que haya un Error en los datos Ingresados, favor verifique"
-//     );
-//   }
+  //   var montoGratificacion = 0;
+  //   if (gratificacion != "no") {
+  //     montoGratificacion = calcularGratificacion(sueldoBruto);
+  //   }
+  //   var bonoMov = parseInt(document.getElementById("bonoMovilizacion").value);
+  //   var bonoCol = parseInt(document.getElementById("bonoColacion").value);
+  //   var afp = parseFloat(document.getElementById("afp").value).toFixed(2) / 100;
+  //   afp = (afp + 10) / 100;
+  //   var prevision = document.getElementById("prevision").value;
+  //   var contrato = document.getElementById("tipocontrato").value;
+
+  var basicoXHs = cantHs * calculoBasico.pagoXHs;
+  let bonificacionZona = basicoXHs * calculoZona.bonificacionZona;
+  //   var descuentosLegales = basicoXHs * afp + basicoXHs * 0.07;
+  //   var sueldoLiquido = parseInt(basicoXHs - descuentosLegales);
+  //   if (sueldoLiquido < 100000) {
+  //     alert(
+  //       "Es posible que haya un Error en los datos Ingresados, favor verifique"
+  //     );
+  //   }
 
   return {
-    imponible: sueldoImponible,
+    imponible: basicoXHs,
     // descuentos: descuentosLegales,
     // liquido: sueldoLiquido
   };
