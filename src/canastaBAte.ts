a9e5749b4dfa30ed6be2b2705a69238fd0fa2023
GonzaloDelCastello <141686474+GonzaloDelCastello@users.noreply.github.com>
2026-07-16 17:22:03 -0300

    historial can basica ate

---
+++ src/canastaBAte.ts
@@ -0,0 +1,43 @@
+export interface CanastaBAte {
+    fecha: string; // aaaa-mm
+    canastaBasica: number;
+}
+
+export const HISTORIAL_C_BASICA_ATE: CanastaBAte[] = [
+    // 2023
+    { fecha: "2023-06", canastaBasica: 363570 },
+    { fecha: "2023-07", canastaBasica: 384657 },
+    { fecha: "2023-08", canastaBasica: 436602 },
+    { fecha: "2023-09", canastaBasica: 494506 },
+    { fecha: "2023-10", canastaBasica: 534498 },
+    { fecha: "2023-11", canastaBasica: 608392 },
+    { fecha: "2023-12", canastaBasica: 772711 },
+    { fecha: "2024-01", canastaBasica: 931323 },
+    { fecha: "2024-02", canastaBasica: 1049820 },
+    { fecha: "2024-03", canastaBasica: 1163391 },
+    { fecha: "2024-04", canastaBasica: 1253953 },
+    { fecha: "2024-05", canastaBasica: 1309303 },
+    { fecha: "2024-06", canastaBasica: 1362029 },
+    { fecha: "2024-07", canastaBasica: 1412647 },
+    { fecha: "2024-08", canastaBasica: 1468989 },
+    { fecha: "2024-09", canastaBasica: 1514208 },
+    { fecha: "2024-10", canastaBasica: 1547169 },
+    { fecha: "2024-11", canastaBasica: 1576284 },
+    { fecha: "2024-12", canastaBasica: 1616148 },
+    { fecha: "2025-01", canastaBasica: 1649499 },
+    { fecha: "2025-02", canastaBasica: 1693576 },
+    { fecha: "2025-03", canastaBasica: 1768976 },
+    { fecha: "2025-04", canastaBasica: 1819121 },
+    { fecha: "2025-05", canastaBasica: 1840098 },
+    { fecha: "2025-06", canastaBasica: 1869924 },
+    { fecha: "2025-07", canastaBasica: 1906092 },
+    { fecha: "2025-08", canastaBasica: 1939698 },
+    { fecha: "2025-09", canastaBasica: 1979769 },
+    { fecha: "2025-10", canastaBasica: 2027283 },
+    { fecha: "2025-11", canastaBasica: 2082071 },
+    { fecha: "2025-12", canastaBasica: 2136860 },
+    { fecha: "2026-01", canastaBasica: 2213476 },
+    { fecha: "2026-02", canastaBasica: 2274094 },
+    { fecha: "2026-03", canastaBasica: 2347463 },
+    { fecha: "2026-04", canastaBasica: 2400186 }
+];
